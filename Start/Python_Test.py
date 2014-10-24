0b1f098092c2833c1958c21e9d5080e2280c18d0
Hannes Heinemann <hheinema@stpool03.(none)>
2014-10-24 13:31:29 +0200

    Python3 verwenden. Nutzlose Kommentarzeilen entfernt

--- Start/Python_Test.py
+++ Start/Python_Test.py
@@ -1,37 +1,22 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
-#  untitled.py
+#  Python_Test.py
 #  
-#  Copyright 2014 Hannes Heinemann <hheinema@localhost>
-#  
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
-#  (at your option) any later version.
-#  
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#  
-#  You should have received a copy of the GNU General Public License
-#  along with this program; if not, write to the Free Software
-#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
-#  MA 02110-1301, USA.
-#  
-#  
+#  2014_10_23 Hannes Heinemann
 
+     #####     #####     #####     #####     #####     #####     #####     ####
 import numpy as np
 
 
 def main():
 
-	a = np.sqrt(4)
-	print("äüöß")
-	
-	return 0
+    a = np.sqrt(4)
+    print("äüöß")
+    
+    return 0
+
 
 if __name__ == '__main__':
-	main()
 
+    main()
