f095f08996108789c9168cc709d498d16532a165
Hannes Heinemann <hheinema@stpool03.(none)>
2014-12-11 09:49:51 +0100

    Tests

--- Start/python_lernen.py
+++ Start/python_lernen.py
@@ -1,1 +1,24 @@
+import time
+
 print("Python lernen!")
+
+n= 100000
+
+start_time = time.time()
+l = []
+for i in range(n):
+    l = l + [i * 2]
+print(time.time() - start_time)
+
+start_time = time.time()
+l = []
+for i in range(n):
+    l += [i * 2]
+print(time.time() - start_time)
+
+start_time = time.time()
+l = []
+for i in range(n):
+    l.append(i * 2)
+print(time.time() - start_time)
+
