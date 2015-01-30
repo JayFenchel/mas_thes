eb3a0aa1a6ba072f64e4ee267fcf90a2fa27aea1
JayFenchel <hannes.heinemann@web.de>
2015-01-30 16:18:30 +0100

    neue Klassen aufrufen, sys und QP

--- ModelPredictiveControl/ModelPredictiveControl.py
+++ ModelPredictiveControl/ModelPredictiveControl.py
@@ -4,9 +4,22 @@ __author__ = 'jayf'
 
 import numpy as np
 from MyMath import vector_norm
+from Systems import SimpleExample
+from QuadraticProgram import QuadraticProgram
 
 
-a = np.array([[2], [4], [4]])
 
-print(a)
-print(vector_norm(a))
+sys = SimpleExample()
+QP = QuadraticProgram(sys)
+
+n = sys.n
+m = sys.m
+T = sys.T  # Planning horizon
+
+x0 = np.array([[0], [1], [0]])
+QP.solve(x0)
+
+
+
+# print(QP.C)
+
