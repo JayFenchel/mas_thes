bd3b432615019237332915d27a5f0ee807fa57ef
JayFenchel <hannes.heinemann@web.de>
2015-01-30 09:58:05 +0100

    Neue Aufgabe: Interior Point Method speziell für MPC

---
+++ ModelPredictiveControl/ModelPredictiveControl.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+__author__ = 'jayf'
+
+import numpy as np
+from MyMath import vector_norm
+
+
+a = np.array([[2], [4], [4]])
+
+print(a)
+print(vector_norm(a))
---
+++ ModelPredictiveControl/MyMath.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+__author__ = 'jayf'
+
+import numpy as np
+
+
+def vector_norm(a):
+    n = 0
+    for i in range(0, a.shape[0]):
+        n += a[i]**2
+    return np.sqrt(n)
