3d730653d07fbe122b0c4b0403b50b9076d4d1b0
JayFenchel <hannes.heinemann@web.de>
2015-06-26 11:44:03 +0200

    mit -c_max, werden lösungen gefunden, die die conic constraints nicht verletzen möglichkeit norm ohne wurzelziehen zu verwenden

--- ModelPredictiveControl/data/nonlcon.m
+++ ModelPredictiveControl/data/nonlcon.m
@@ -3,11 +3,17 @@ function [ c, ceq ] = nonlcon( U )
 %   Detailed explanation goes here
 load socp_matrices.mat
 
-c(1) = norm(V21*U) + M21*U + c_max;
-c(2) = norm(V22*U) + M22*U + c_max;
-c(3) = norm(V23*U) + M23*U + c_max;
-c(4) = norm(V24*U) + M24*U + c_max;
-c(5) = norm(V25*U) + M25*U + c_max;
+c(1) = norm(V21*U) + M21*U - c_max;
+c(2) = norm(V22*U) + M22*U - c_max;
+c(3) = norm(V23*U) + M23*U - c_max;
+c(4) = norm(V24*U) + M24*U - c_max;
+c(5) = norm(V25*U) + M25*U - c_max;
+
+% c(1) = (sum((V21*U).*(V21*U))) + M21*U + c_max;
+% c(2) = (sum((V22*U).*(V22*U))) + M22*U + c_max;
+% c(3) = (sum((V23*U).*(V23*U))) + M23*U + c_max;
+% c(4) = (sum((V24*U).*(V24*U))) + M24*U + c_max;
+% c(5) = (sum((V25*U).*(V25*U))) + M25*U + c_max;
 
 ceq = 0;
 
